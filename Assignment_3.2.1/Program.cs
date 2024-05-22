4817a91829e818c235d09d8bfe7aba2f198f6cb4
flowmellow <michael.floresmmt@gmail.com>
2024-05-22 10:16:39 -0400

    Add project files.

---
+++ Assignment_3.2.1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Create a 2D array to store integers and print them in matrix format with proper formatting.
+e. g:
+| 2 | 3 | 4 |
+| 1 | 4 | 6 |
+*/
+
+namespace Assignment_3._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrixArray = { { 2, 3, 4 }, { 1, 4, 6 } };
+
+            for (int i = 0; i < matrixArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixArray.GetLength(1); j++)
+                {
+
+                    Console.Write("|");
+                    Console.Write(matrixArray[i,j]);
+                }
+                Console.Write("|");
+                Console.WriteLine();
+
+
+            }
+
+
+        }
+    }
+}
